55c9037c86420c1e7b7a0ee6ad077a141b459e82
Ahmed Khanzada <ahmed-raza-khanzada@github.com>
2023-05-29 19:52:09 +0500

    least-final

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import csv
 from datetime import datetime, timedelta
-
+import pandas as pd
 def save_customer_data(name):
     current_datetime = datetime.now()
     current_year = current_datetime.year
@@ -8,29 +8,30 @@ def save_customer_data(name):
     current_time = current_datetime.time().strftime("%H:%M")
 
     data = [current_year, current_date, current_time, name]
-
-    last_entry_time, last_entry_name = get_last_entry()
-    print(current_datetime, name, last_entry_time, last_entry_name, "********")
+    header = ["year", "date", "time", "Customer_id"]
+    last_entry_time, last_entry_name = get_last_entry(name)
+    # print(current_datetime, name, last_entry_time, last_entry_name, "********")
     if (last_entry_time is None) or ((current_datetime - last_entry_time) >= timedelta(hours=1)) or (name != last_entry_name):
-        with open('customer_data.csv', 'a', newline='') as csvfile:
+        with open('static/customer_data.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
+            if csvfile.tell() == 0:
+                writer.writerow(header)
             writer.writerow(data)
 
-def get_last_entry():
+def get_last_entry(name):
     last_entry_time = None
     last_entry_name = None
 
     try:
-        with open('customer_data.csv', 'r') as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                if len(row) >= 4:
-                    row_date = datetime.strptime(row[1], "%Y-%m-%d")
-                    row_time = datetime.strptime(row[2], "%H:%M").time()
-                    row_datetime = datetime.combine(row_date, row_time)
-                    if last_entry_time is None or row_datetime > last_entry_time:
-                        last_entry_time = row_datetime
-                        last_entry_name = row[3]
+        df = pd.read_csv("static/customer_data.csv",names = ["year","date","time","Customer_id"])
+        n = df[df["Customer_id"]==name]
+        # print("#############",n)
+        if len(n)>0:
+            row_date = datetime.strptime(list(n["date"])[-1], "%Y-%m-%d")
+            row_time = datetime.strptime(list(n["time"])[-1], "%H:%M").time()
+            row_datetime = datetime.combine(row_date, row_time)
+            last_entry_time = row_datetime
+            last_entry_name = name
     except (FileNotFoundError, IndexError, ValueError):
         pass
 
